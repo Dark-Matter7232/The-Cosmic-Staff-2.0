4ef5b31e9ce9e7092e101d86a1c1bf9cfb8878f0
ichibauer <evanf780@gmail.com>
2021-09-23 20:59:27 +0700

    Revert "fs: Implement Dynamic FSync 2.0"
    This reverts commit 2c38c8915905981c5bcc93b12794a0fb9e8109af.

--- fs/sync.c
+++ fs/sync.c
@@ -22,10 +22,6 @@
 bool fsync_enabled = true;
 module_param(fsync_enabled, bool, 0644);
 
-#ifdef CONFIG_DYNAMIC_FSYNC
-#include <linux/dyn_sync_cntrl.h>
-#endif
-
 #define VALID_FLAGS (SYNC_FILE_RANGE_WAIT_BEFORE|SYNC_FILE_RANGE_WRITE| \
 			SYNC_FILE_RANGE_WAIT_AFTER)
 
@@ -353,30 +349,6 @@ static void fdatawait_one_bdev(struct block_device *bdev, void *arg)
 	filemap_fdatawait_keep_errors(bdev->bd_inode->i_mapping);
 }
 
-#ifdef CONFIG_DYNAMIC_FSYNC
-/*
- * Sync all the data for all the filesystems (called by sys_sync() and
- * emergency sync)
- */
-void sync_filesystems(int nowait)
-{
-	/*
-	 * Sync twice to reduce the possibility we skipped some inodes / pages
-	 * because they were temporarily locked
-	 */
-
-	iterate_supers(sync_inodes_one_sb, &nowait);
-	iterate_supers(sync_fs_one_sb, &nowait);
-	iterate_bdevs(fdatawrite_one_bdev, NULL);
-	iterate_bdevs(fdatawait_one_bdev, NULL);
-
-	iterate_supers(sync_inodes_one_sb, &nowait);
-	iterate_supers(sync_fs_one_sb, &nowait);
-	iterate_bdevs(fdatawrite_one_bdev, NULL);
-	iterate_bdevs(fdatawait_one_bdev, NULL);
-}
-#endif
-
 /*
  * Sync everything. We start by waking flusher threads so that most of
  * writeback runs on all devices in parallel. Then we sync all inodes reliably
@@ -472,11 +444,6 @@ int vfs_fsync_range(struct file *file, loff_t start, loff_t end, int datasync)
 {
 	struct inode *inode = file->f_mapping->host;
 
-#ifdef CONFIG_DYNAMIC_FSYNC
-	if (dyn_fsync_active && suspend_active)
-		return 0;
-#endif
-
 	if (!fsync_enabled)
 		return 0;
 
@@ -529,10 +496,6 @@ static int do_fsync(unsigned int fd, int datasync)
 
 SYSCALL_DEFINE1(fsync, unsigned int, fd)
 {
-#ifdef CONFIG_DYNAMIC_FSYNC
-	if (dyn_fsync_active && suspend_active)
-		return 0;
-#endif
 	if (!fsync_enabled)
 		return 0;
 
@@ -541,10 +504,6 @@ SYSCALL_DEFINE1(fsync, unsigned int, fd)
 
 SYSCALL_DEFINE1(fdatasync, unsigned int, fd)
 {
-#ifdef CONFIG_DYNAMIC_FSYNC
-	if (dyn_fsync_active && suspend_active)
-		return 0;
-#endif
 	if (!fsync_enabled)
 		return 0;
 
@@ -606,11 +565,6 @@ SYSCALL_DEFINE4(sync_file_range, int, fd, loff_t, offset, loff_t, nbytes,
 	struct address_space *mapping;
 	loff_t endbyte;			/* inclusive */
 	umode_t i_mode;
-
-#ifdef CONFIG_DYNAMIC_FSYNC
-	if (dyn_fsync_active && suspend_active)
-		return 0;
-#endif
 
 	if (!fsync_enabled)
 		return 0;
@@ -690,9 +644,5 @@ out:
 SYSCALL_DEFINE4(sync_file_range2, int, fd, unsigned int, flags,
 				 loff_t, offset, loff_t, nbytes)
 {
-#ifdef CONFIG_DYNAMIC_FSYNC
-	if (dyn_fsync_active && suspend_active)
-		return 0;
-#endif
 	return sys_sync_file_range(fd, offset, nbytes, flags);
 }
