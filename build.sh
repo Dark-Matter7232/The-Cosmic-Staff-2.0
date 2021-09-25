9dc87869f9eb2499fd5d649fd3203c9676d8aad1
ichibauer <evanf780@gmail.com>
2021-09-25 15:03:00 +0700

    build.sh: little revamp

--- build.sh
+++ build.sh
@@ -2,11 +2,12 @@
 
 BUILD_DATE=$(date +"%m-%d-%Y")
 
-TC_DIR="/tc/protoC/bin"
-ZIP_DIR="/home/ichibauer/kernelBuilding/zippy"
-IMG_DIR="/home/ichibauer/kernelBuilding/imageyy/AIK-Linux"
-OUT_IMG_DIR="/home/ichibauer/kernelBuilding/optio/out/arch/arm64/boot"
-FINAL_DIR="/home/ichibauer/kernelBuilding/feenal"
+# Directory variables
+TC_DIR="/tc/protoC/bin"							 # Toolchain/compiler directory
+ZIP_DIR="/home/ichibauer/kernelBuilding/zippy"				 # AnyKernel3 directory
+IMG_DIR="/home/ichibauer/kernelBuilding/imageyy/AIK-Linux"               # AIK directory
+OUT_IMG_DIR="/home/ichibauer/kernelBuilding/optio/out/arch/arm64/boot"   # Raw/compiled Image directory
+FINAL_DIR="/home/ichibauer/kernelBuilding/feenal"                        # Where flashable image/zip is stored 
 
 export ARCH=arm64
 export SUBARCH=arm64
@@ -16,96 +17,144 @@ export PLATFORM_VERSION=11
 export KBUILD_BUILD_USER=$(whoami)
 export KBUILD_BUILD_HOST=$(hostname)
 
-if [ -d $(pwd)/out ]
-then
-    read -p "Build clean (y/n)? " cleanOrNo
-    echo " "
-    if [[ $cleanOrNo == "y" || $cleanOrNo == "Y" ]]
+exitScript()
+{
+    exit 1
+}
+
+checkImage()
+{
+    if [ -e ${OUT_IMG_DIR}/Image ]
     then
-        make O=out distclean
-    fi
-fi
-
-clear
-
-make O=out M21_defconfig
-time make O=out -j$(nproc --all) 2>&1 | tee log.txt
-
-echo " "
-read -p "Zip (1) or Image (2)? " selection
-echo " "
-read -p "Enter name (no spaces): " fileName
-echo " "
-
-KERNEL_FILE=${fileName}-${BUILD_DATE}
-
-if [ $selection == "1" ]
-then
-    echo " "
-    cd ${ZIP_DIR}
-    rm -f *.zip
-    rm -f Image
-    printf "Optio-\n" | figlet > version
-    printf "\n Build date: ${BUILD_DATE}\n" >> version
-    mv ${OUT_IMG_DIR}/Image ${ZIP_DIR}
-    zip -r9 ${KERNEL_FILE}.zip *
-    mv ${KERNEL_FILE}.zip ${FINAL_DIR}
-    
-    echo " "
-    read -p "Want to reboot to recovery (y/n)? " bootToRecovery
-
-    if [[ $bootToRecovery == "y" || $bootToRecovery == "Y" ]]
-    then
-	    COPY_TO_DIR="/sdcard/Download"
-
-        cd ${FINAL_DIR}
-        adb push ${KERNEL_FILE}.zip ${COPY_TO_DIR}
-        adb reboot recovery
+        printf "\n"
+        printf "Found raw Image/kernel at ${OUT_IMG_DIR}/Image\n"
+        printf "\n"
     else
-        echo " "
-        echo "Zip can be found at ${FINAL_DIR}/${KERNEL_FILE}.zip"
-        echo " "
+        printf "\n"
+        printf "Raw Image/kernel not found; see log.txt for details.\n"
+        printf "\n"
+
+        exitScript
     fi
+}
 
-elif [ $selection == "2" ]
-then
-    IMG_DEFAULT="stock"
+packKernel()
+{
+    checkImage
 
-    echo " "
-    cd ${IMG_DIR}
-    bash cleanup.sh
-    bash unpackimg.sh
-    mv ${OUT_IMG_DIR}/Image ${IMG_DIR}/split_img/
-    rm -f split_img/${IMG_DEFAULT}.img-kernel
-    mv split_img/Image split_img/${IMG_DEFAULT}.img-kernel
-    bash repackimg.sh
-    mv image-new.img ${KERNEL_FILE}.img
-    mv ${KERNEL_FILE}.img ${FINAL_DIR}
+    read -p "Zip (1) or Image (2)? " selection
 
-    echo " "
-    read -p "Want to flash directly to the phone (y/n)? " flashOrNO
-
-    if [[ $flashOrNO == "y" || $flashOrNO == "Y" ]]
+    if ! [[ $selection == "1" || $selection == "2" ]]
     then
-        cd ${FINAL_DIR}
-        adb reboot download
-        echo " "
-        echo "Booting into download mode . . . "
-        sleep 7
-        echo " "
-        echo "Press Enter key to FLASH . . . "
-        read
-        echo "Press Enter key to FLASH . . . [2] "
-        read
-        sudo heimdall flash --BOOT ${KERNEL_FILE}.img
-    else
-        echo " "
-        echo "Image can be found at ${FINAL_DIR}/${KERNEL_FILE}.img"
-        echo " "
+        printf "\n"
+        printf "Invalid input.\n"
+        printf "\n"
+
+        exitScript
     fi
 
-else
-    echo " "
-    echo "Uh oh . . . "
-    echo " "
-fi
+    printf "\n"
+    read -p "Enter name (no spaces): " fileName
+
+    KERNEL_FILE=${fileName}-${BUILD_DATE}
+
+    if [ $selection == "1" ]
+    then
+        printf "\n"
+        cd ${ZIP_DIR}
+
+        rm -f *.zip
+        rm -f Image
+
+        printf "Optio-\n" | figlet > version
+        printf "\n Build date: ${BUILD_DATE}\n" >> version
+
+        mv ${OUT_IMG_DIR}/Image ${ZIP_DIR}
+        zip -r9 ${KERNEL_FILE}.zip *
+        mv ${KERNEL_FILE}.zip ${FINAL_DIR}
+        
+        printf "\n"
+        read -p "Want to reboot to recovery (y/n)? " bootToRecovery
+
+        if [[ $bootToRecovery == "y" || $bootToRecovery == "Y" ]]
+        then
+            COPY_TO_DIR="/sdcard/Download"
+
+            cd ${FINAL_DIR}
+            adb push ${KERNEL_FILE}.zip ${COPY_TO_DIR}
+            adb reboot recovery
+        else
+            printf "\n"
+            printf "Zip can be found at ${FINAL_DIR}/${KERNEL_FILE}.zip\n"
+            printf "\n"
+        fi
+    elif [ $selection == "2" ]
+    then
+        IMG_DEFAULT="stock"
+        printf "\n"
+
+        cd ${IMG_DIR}
+        bash cleanup.sh
+        bash unpackimg.sh
+
+        mv ${OUT_IMG_DIR}/Image ${IMG_DIR}/split_img/
+        rm -f split_img/${IMG_DEFAULT}.img-kernel
+        mv split_img/Image split_img/${IMG_DEFAULT}.img-kernel
+
+        bash repackimg.sh
+        mv image-new.img ${KERNEL_FILE}.img
+        mv ${KERNEL_FILE}.img ${FINAL_DIR}
+
+        printf "\n"
+        read -p "Want to flash directly to the phone (y/n)? " flashOrNO
+
+        if [[ $flashOrNO == "y" || $flashOrNO == "Y" ]]
+        then
+            cd ${FINAL_DIR}
+            adb reboot download
+
+            printf "\n"
+            printf "Booting into download mode . . . \n"
+
+            sleep 7
+
+            printf "\n"
+            printf "Press Enter key to FLASH . . . \n"
+            read
+            printf "Press Enter key to FLASH . . . [2] \n"
+            read
+
+            sudo heimdall flash --BOOT ${KERNEL_FILE}.img
+        else
+            printf "\n"
+            printf "Image can be found at ${FINAL_DIR}/${KERNEL_FILE}.img\n"
+            printf "\n"
+        fi
+    fi
+
+   exitScript
+}
+
+compileKernel()
+{
+    DEVICE_DEFCONFIG="M21_defconfig"
+
+    if [ -d $(pwd)/out ]
+    then
+        read -p "Build clean (y/n)? " cleanOrNo
+        printf "\n"
+        if [[ $cleanOrNo == "y" || $cleanOrNo == "Y" ]]
+        then
+            make O=out distclean
+        fi
+    fi
+
+    clear
+
+    make O=out ${DEVICE_DEFCONFIG}
+    time make O=out -j$(nproc --all) 2>&1 | tee log.txt
+
+    packKernel
+}
+
+compileKernel
