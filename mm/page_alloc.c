8a69bbb1fd6d59f0164317a975d40a88ee380019
ichibauer <evanf780@gmail.com>
2021-09-29 11:24:38 +0700

    mm: fix missing function body

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -3684,8 +3684,7 @@ retry:
 	 */
 	if (!page && !drained) {
 		unreserve_highatomic_pageblock(ac, false);
-		if (!need_memory_boosting(NULL))
-			drain_all_pages(NULL);
+		drain_all_pages(NULL);
 		drained = true;
 		goto retry;
 	}
