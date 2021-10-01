e803756d52f7bc9b761b10b0b2152998a84301af
Panchajanya1999 <panchajanya@azure-dev.live>
2021-10-01 21:21:08 +0530

    cfq-iosched: Apply RedHat suggested tunings for fast storage
    Apply tunings from redHat[1] for faster storage.
    
    [1]: https://access.redhat.com/documentation/en-us/red_hat_enterprise_linux/7/pdf/performance_tuning_guide/red_hat_enterprise_linux-7-performance_tuning_guide-en-us.pdf
    (Section 8.4.5.1)
    
    Change-Id: Ie8e3f7898d661362212d3eac5fe854362048ce94
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>

--- block/cfq-iosched.c
+++ block/cfq-iosched.c
@@ -4748,7 +4748,7 @@ SHOW_FUNCTION(cfq_back_seek_max_show, cfqd->cfq_back_max, 0);
 SHOW_FUNCTION(cfq_back_seek_penalty_show, cfqd->cfq_back_penalty, 0);
 SHOW_FUNCTION(cfq_slice_idle_show, cfqd->cfq_slice_idle, 1);
 SHOW_FUNCTION(cfq_max_async_dispatch_show, cfqd->cfq_max_async_dispatch, 0);
-SHOW_FUNCTION(cfq_group_idle_show, cfqd->cfq_group_idle, 1);
+SHOW_FUNCTION(cfq_group_idle_show, cfqd->cfq_group_idle, 0);
 SHOW_FUNCTION(cfq_rt_idle_only_show, cfqd->cfq_rt_idle_only, 0);
 SHOW_FUNCTION(cfq_slice_sync_show, cfqd->cfq_slice[1], 1);
 SHOW_FUNCTION(cfq_slice_async_show, cfqd->cfq_slice[0], 1);
