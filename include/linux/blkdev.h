ea28cb3e780b152b7bb46247d31910078e9bbed1
ichibauer <evanf780@gmail.com>
2021-10-05 23:13:58 +0700

    fs: Import NTFS3 for 4.14
    This commit include
    Source: https://github.com/LGA1150/ntfs3-oot.git
    Patch: https://www.lkml.org/lkml/2019/7/19/745
    
    Signed-off-by: ichibauer <evanf780@gmail.com>

--- include/linux/blkdev.h
+++ include/linux/blkdev.h
@@ -5,6 +5,19 @@
 #include <linux/sched.h>
 #include <linux/sched/clock.h>
 
+/*
+ * The basic unit of block I/O is a sector. It is used in a number of contexts
+ * in Linux (blk, bio, genhd). The size of one sector is 512 = 2**9
+ * bytes. Variables of type sector_t represent an offset or size that is a
+ * multiple of 512 bytes. Hence these two constants.
+ */
+#ifndef SECTOR_SHIFT
+#define SECTOR_SHIFT 9
+#endif
+#ifndef SECTOR_SIZE
+#define SECTOR_SIZE (1 << SECTOR_SHIFT)
+#endif
+
 #ifdef CONFIG_BLOCK
 
 #include <linux/major.h>
