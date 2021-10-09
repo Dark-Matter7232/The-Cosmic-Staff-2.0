a9fa6e296102c533deda0539bd46441f59ab63a6
gamer13433 <saintpi70@gmail.com>
2021-10-09 18:33:37 +0700

    disable some debug
    Signed-off-by: ichibauer <evanf780@gmail.com>

--- drivers/battery_v2/battery_notifier.c
+++ drivers/battery_v2/battery_notifier.c
@@ -24,7 +24,7 @@
 #include <linux/battery/battery_notifier.h>
 #include <linux/sec_class.h>
 
-#define DEBUG
+//#define DEBUG
 #define SET_BATTERY_NOTIFIER_BLOCK(nb, fn, dev) do {	\
 		(nb)->notifier_call = (fn);		\
 		(nb)->priority = (dev);			\
